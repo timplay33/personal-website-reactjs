8d3e115070c5aee8fc12e609960e938071aca382
timplay33 <tim3play3@gmail.com>
2023-04-01 21:03:51 +0200

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ function Form(){
   return (
     <div className="form">
       <h1>Contact</h1>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="POST" data-netlify="true" netlify>
         <p>
           <label>Your Name: <input type="text" name="name" /></label>
         </p>
